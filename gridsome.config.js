6bff896aff3db29c6d9ff247e79b0e5a9d329cdd
ideas24h <raul@ideas24h.com>
2019-12-10 13:48:01 +0100

    cambio ruta del sitio.

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,7 +7,7 @@ class TailwindExtractor {
 module.exports = {
   siteName: 'Navidades Online',
   siteDescription: "Navidad is Coming. Ideas para decorar tu Navidad",
-  siteUrl: 'https://gridsome-starter-bleda.netlify.com',
+  siteUrl: 'https://navidades.online/',
   titleTemplate: `%s | Navidad is coming`,
   icon: 'src/favicon.png',
 
@@ -59,13 +59,13 @@ module.exports = {
         contentTypeName: 'Post',
         feedOptions: {
           title: 'Bleda, a Gridsome blog starter',
-          feed_url: 'https://gridsome-starter-bleda.netlify.com/feed.xml',
-          site_url: 'https://gridsome-starter-bleda.netlify.com',
+          feed_url: 'https://navidades.online//feed.xml',
+          site_url: 'https://navidades.online/',
         },
         feedItemOptions: node => ({
           title: node.title,
           description: node.description,
-          url: 'https://gridsome-starter-bleda.netlify.com/' + node.slug,
+          url: 'https://navidades.online//' + node.slug,
           author: node.author,
           date: node.date,
         }),
